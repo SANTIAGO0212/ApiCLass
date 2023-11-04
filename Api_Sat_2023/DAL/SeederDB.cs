4539c9fbd7778c279818a69029c6e6897b188e47
Santiago Jimenez <santiagocj0212@gmail.com>
2023-11-04 12:36:33 -0500

    Commit SeederDB

---
+++ Api_Sat_2023/DAL/SeederDB.cs
@@ -0,0 +1,77 @@
+﻿using Api_Sat_2023.DAL.Entities;
+
+namespace Api_Sat_2023.DAL
+{
+    public class SeederDB
+    {
+        private readonly DateBaseContext _context;
+        public SeederDB(DateBaseContext context)
+        {
+            _context = context;
+        }
+
+        //Crearemos un método llamado Seeder Async
+        //Este método es una especie de MAIN()
+        //Este método tendrá la responsabilidad de prepoblar las tablas de mi BD
+
+        public async Task SeederAsync()
+        {
+            //Primero: Agregaré un método propio de EF que hace las veces de comando 'update-database'
+            //En otras palabras: un método creará la BD inmediatamente ponga en ejecucuión mi API
+            await _context.Database.EnsureCreatedAsync();
+
+            //A partir de aquí vamos a ir creando métodos que me sirvan para pepobrar mi BD
+            await PopulateCountriesAsync();
+            await _context.SaveChangesAsync(); //Esta línea me guarda ls datos en BD
+        }
+
+        private async Task PopulateCountriesAsync()
+        {
+            //El método Any() me indica si la tabla Countries tiene al menos un registro
+            //El método Any negado (!) me indica que no hay ningun registro en mi tabla countries
+            if (!_context.Countries.Any())
+            {
+                //Así creo yo un objeto país con sus respectivos estados
+                _context.Countries.Add(new Country
+                {
+                    CreatedDate = DateTime.Now,
+                    Name = "Colombia",
+                    States = new List<State>()
+                    {
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Antioquia"
+                        },
+
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Cundinamarca"
+                        }
+                    }
+                });
+                _context.Countries.Add(new Country
+                {
+                    CreatedDate = DateTime.Now,
+                    Name = "Argentina",
+                    States = new List<State>()
+                    {
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Buenos Aires"
+                        },
+
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Ezeiza"
+                        }
+                    }
+                });
+
+            }
+        }
+    }
+}
--- Api_Sat_2023/Program.cs
+++ Api_Sat_2023/Program.cs
@@ -13,11 +13,25 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DateBaseContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<ICountryService, CountryService>(); //Por cada nuevo servicio de iterfaz que yo genero en mi API +
                                                                //debo agregar l anueva dependencia 
+builder.Services.AddScoped<IStateService, StateService>();
+builder.Services.AddTransient<SeederDB>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+SeederData();
+void SeederData()
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (IServiceScope? scope = scopedFactory.CreateScope())
+    {
+        SeederDB? service = scope.ServiceProvider.GetService<SeederDB>();
+        service.SeederAsync().Wait();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
